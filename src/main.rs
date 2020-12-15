3546f1eb73f10e6f2a49b25f09c480523655a551
qquan <1070657779@qq.com>
2020-12-15 16:20:37 +0800

    a2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+extern crate regex;
+use regex::Regex;
+use enigo::*;
+use clipboard_win::{formats, get_clipboard, set_clipboard};
 fn main() {
-    let _a1: i32=23;
-    let a4=54;
-    let m34=8.73;
-    println!("Hello, world!");
-    let s2:i32 =65;
+  let re=Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+  println!("Did our date match? {}",re.is_match("2014-01-01"));
+  let text = "my sample ><";
+    
+    set_clipboard(formats::Unicode, text).expect("To set clipboard");
+    //Type is necessary as string can be stored in various storages
+    let result: String = get_clipboard(formats::Unicode).expect("To set clipboard");
+    assert_eq!(result, text);
+    let mut enigo=Enigo::new();
+    enigo.mouse_move_to(1244, 134);
+    enigo.mouse_down(MouseButton::Right);
+    enigo.mouse_up(MouseButton::Right);
 }
